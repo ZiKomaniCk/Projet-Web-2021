f8cfbef115002118a9680ff708739e5e0577dab9
Modolo Thomas <thomas.modolo@ynov.com>
2021-01-07 16:15:27 +0100

    correcting syntaxe

--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Genre::class, function (Faker $faker) {
     return [
         'name' => $faker->randomElement(['Action' ,'Aventure', 'Role-playing', 'MMO', 'Simulation', 'Sports', 'Sandbox', 'Horreur']),
-        'type' => $faker->randomElement(['FPS' ,'TPS', 'Survie', 'Battle Royal', 'Roguelikes', 'Courses', 'Tower defense', 'Combat', 'Tour par tour', 'Creatif']),
+        'type' => $faker->randomElement(['FPS' ,'TPS', 'Survie', 'Battle Royal', 'Roguelikes', 'Courses', 'Tower defense', 'Combat', 'Tour par tour', 'Créatif']),
     ];
 });
