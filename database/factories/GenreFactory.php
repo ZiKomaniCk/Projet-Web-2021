fec8c0daf69404ec85c6b4def3ec58f85d891aca
Modolo Thomas <thomas.modolo@ynov.com>
2021-01-07 16:14:19 +0100

    adding genre table

---
+++ database/factories/GenreFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Genre;
+use Faker\Generator as Faker;
+
+$factory->define(Genre::class, function (Faker $faker) {
+    return [
+        'name' => $faker->randomElement(['Action' ,'Aventure', 'Role-playing', 'MMO', 'Simulation', 'Sports', 'Sandbox', 'Horreur']),
+        'type' => $faker->randomElement(['FPS' ,'TPS', 'Survie', 'Battle Royal', 'Roguelikes', 'Courses', 'Tower defense', 'Combat', 'Tour par tour', 'Creatif']),
+    ];
+});
