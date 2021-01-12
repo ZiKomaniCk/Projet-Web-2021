bc18512c10b975e3d344363c0546a99ef021aaef
ZiKo <lucas.matas@ynov.com>
2021-01-12 15:27:46 +0100

    commit change excuse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                     {{ __('You are logged in!') }}
                     
 
-                    <p>baise tes moers</p>
+                    <p>pardon</p>
                 </div>
             </div>
         </div>
