6fd498145f8e598efc19d8d1cd7a05a95fb81d8c
ZiKo <lucas.matas@ynov.com>
2021-01-14 11:19:44 +0100

    card change home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,27 +24,34 @@
 </div>
 
 <div class="container">
-  <div class="card mb-3" style="max-width: 100%;">
+  <div class="card mb-3">
     <div class="row g-0">
       <div class="col-md-4">
         <img src="{{$user->imgPath}}" class="profilePicture" style="max-width: 250px;" alt="Votre photo de profil">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title fs-1 fw-bold">{{$user->nickname}}</h5>
-          <br>
-          <p class="card-text fs-5">Informations personelles</p>
-          <p class="card-text fs-5">Nom : {{$user->lastName}}</p>
-          <p class="card-text fs-5">Prénom : {{$user->firstName}}</p>
-          <p class="card-text fs-5">Nom : {{$user->lastName}}</p>
-          <p class="card-text fs-5">Nom : {{$user->lastName}}</p>
-
-
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <div class="card-body">
+          <h5 class="card-title fs-1 fw-bold">{{$user->nickname}}</h5> 
+          <hr>
+          <h5 class="text-end fs-3" style="margin-bottom">crédit: {{$user->credit}}</h5>
         </div>
       </div>
     </div>
   </div>
 </div>
+</div>
 
+<div class="container">
+<div class="card" style="width: 25rem;">
+  <div class="card-body text-center">
+    <h5 class="card-title fs-4">Information personnelles</h5>
+    
+    <p class="card-text fs-6">Nom : {{$user->lastName}}</p>
+    <p class="card-text fs-6">Prénom : {{$user->firstName}}</p>
+    <p class="card-text fs-6">Email : {{$user->email}}</p>
+    <p class="card-text fs-6">Nom : {{$user->lastName}}</p>
+  </div>
+</div>
+</div>
 @endsection
