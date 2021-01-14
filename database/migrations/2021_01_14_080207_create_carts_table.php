2759c91a62623ce3a0da8fa84c68300a7f6dc3f6
Modolo Thomas <thomas.modolo@ynov.com>
2021-01-14 09:14:19 +0100

    adding migration

--- database/migrations/2021_01_14_080207_create_carts_table.php
+++ database/migrations/2021_01_14_080207_create_carts_table.php
@@ -15,6 +15,8 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->integer('game_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
