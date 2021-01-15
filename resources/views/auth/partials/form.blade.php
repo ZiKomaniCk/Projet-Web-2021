46fd7f9866b2fe2f62eb24ba0c14240ec4c588b2
ZiKo <lucas.matas@ynov.com>
2021-01-15 12:16:50 +0100

    Anglais => fr

--- resources/views/auth/partials/form.blade.php
+++ resources/views/auth/partials/form.blade.php
@@ -1,7 +1,20 @@
 
+    <div class="form-group row">
+        <label for="lastName" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+        
+        <div class="col-md-6">
+            <input id="lastName" type="text" class="form-control @error('lastName') is-invalid @enderror" name="lastName" value="{{ $user->lastName ?? '' }}" required autocomplete="lastName" autofocus>
+            
+            @error('lastName')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
 
 <div class="form-group row">
-    <label for="firstName" class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
+    <label for="firstName" class="col-md-4 col-form-label text-md-right">{{ __('Prénom') }}</label>
     
     <div class="col-md-6">
         <input id="firstName" type="text" class="form-control @error('firstName') is-invalid @enderror" name="firstName" value="{{ $user->firstName ?? '' }}" required autocomplete="firstName" autofocus>
@@ -14,22 +27,9 @@
     </div>
 </div>
 
-<div class="form-group row">
-    <label for="lastName" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
-    
-    <div class="col-md-6">
-        <input id="lastName" type="text" class="form-control @error('lastName') is-invalid @enderror" name="lastName" value="{{ $user->lastName ?? '' }}" required autocomplete="lastName" autofocus>
-        
-        @error('lastName')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    </div>
-</div>
 
 <div class="form-group row">
-    <label for="nickname" class="col-md-4 col-form-label text-md-right">{{ __('Nickname') }}</label>
+    <label for="nickname" class="col-md-4 col-form-label text-md-right">{{ __('Pseudo') }}</label>
     
     <div class="col-md-6">
         <input id="nickname" type="text" class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{ $user->nickname ?? '' }}" required autocomplete="nickname" autofocus>
@@ -43,7 +43,7 @@
 </div>
 
 <div class="form-group row">
-    <label for="credit" class="col-md-4 col-form-label text-md-right">{{ __('Credit') }}</label>
+    <label for="credit" class="col-md-4 col-form-label text-md-right">{{ __('Solde') }}</label>
     
     <div class="col-md-6">
         <input id="credit" type="number" class="form-control @error('credit') is-invalid @enderror" name="credit" value="{{ $user->credit ?? '' }}" autocomplete="credit" autofocus>
@@ -57,7 +57,7 @@
 </div>
 
 <div class="form-group row">
-    <label for="birthDate" class="col-md-4 col-form-label text-md-right">{{ __('Birth Date') }}</label>
+    <label for="birthDate" class="col-md-4 col-form-label text-md-right">{{ __("Date d'anniversaire") }}</label>
     
     <div class="col-md-6">
         <input id="birthDate" type="date" class="form-control @error('birthDate') is-invalid @enderror" name="birthDate" value="{{ $user->birthDate ?? '' }}" required autocomplete="birthDate">
@@ -71,7 +71,7 @@
 </div>
 
 <div class="form-group row">
-    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Addresse E-Mail ') }}</label>
     
     <div class="col-md-6">
         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email ?? '' }}" required autocomplete="email">
@@ -86,7 +86,7 @@
 
 @if ($routeName == 'register')
 <div class="form-group row">
-    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
     
     <div class="col-md-6">
         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -100,7 +100,7 @@
 </div>
 
 <div class="form-group row">
-    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmer le mot de passe') }}</label>
     
     <div class="col-md-6">
         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -110,7 +110,7 @@
 <div class="form-group row mb-0">
     <div class="col-md-6 offset-md-4">
         <button type="submit" class="btn btn-primary">
-            {{ __('Register') }}
+            {{ __("S'enregistrer") }}
         </button>
     </div>
 </div>
