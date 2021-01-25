3d344a71beb0fb75165bea1a4628a68f23c6b096
ZiKo <lucas.matas@ynov.com>
2021-01-25 18:46:08 +0100

    paiement css

--- resources/views/checkouts/index.blade.php
+++ resources/views/checkouts/index.blade.php
@@ -9,24 +9,29 @@
 @endsection
 
 @section('content')
-    <div class="col-md-12">
-        <h1>Page de paiement</h1>
-        <div class="row">
-            <div class="col-md-6">
-                <form action="{{ route('checkouts.store') }}" method="POST" id="payment-form" class="my-4">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card border-primary">
+                <div class="card-header fs-4 fw-bold text-primary">{{ __('Page de paiement') }}</div>
+                
+                <form action="{{ route('checkouts.store') }}" method="POST" id="payment-form" class="m-5">
                     @csrf
-                    <div id="card-element">
-                    <!-- Elements will create input elements here -->
-                    </div>
-
-                    <!-- We'll put the error messages in this element -->
+                    <span >
+                        <div id="card-element" class=" mt-2">
+                            <!-- Elements will create input elements here -->
+                        </div>
+                    </span>
                     <div id="card-errors" role="alert"></div>
-
-                    <button class="btn btn-success mt-3" id="submit">Procéder au paiement</button>
+                    <!-- We'll put the error messages in this element -->
+                    
+                    <button class="btn btn-success mt-3 mr-3 fs-5 float-right" id="submit">Procéder au paiement</button>
                 </form>
+                
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('extra-js')
@@ -35,23 +40,24 @@
     var elements = stripe.elements();
     var style = {
         base: {
-        color: "#32325d",
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: "antialiased",
-        fontSize: "16px",
-        "::placeholder": {
-            color: "#aab7c4"
-        }
+            color: "#ffffff",
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: "antialiased",
+            fontSize: "25px",
+            iconColor: "#F7F1F0",
+            "::placeholder": {
+                color: "#aab7c4"
+            }
         },
         invalid: {
-        color: "#fa755a",
-        iconColor: "#fa755a"
+            color: "#fa755a",
+            iconColor: "#fa755a"
         }
     };
     var card = elements.create("card", { style: style });
     card.mount("#card-element");
     card.addEventListener('change', ({error}) => {
-    const displayError = document.getElementById('card-errors');
+        const displayError = document.getElementById('card-errors');
         if (error) {
             displayError.classList.add('alert', 'alert-warning', 'mt-3');
             displayError.textContent = error.message;
@@ -62,11 +68,11 @@
     });
     var submitButton = document.getElementById('submit');
     submitButton.addEventListener('click', function(ev) {
-    ev.preventDefault();
-    stripe.confirmCardPayment("{{ $clientSecret }}", {
-        payment_method: {
-            card: card
-        }
+        ev.preventDefault();
+        stripe.confirmCardPayment("{{ $clientSecret }}", {
+            payment_method: {
+                card: card
+            }
         }).then(function(result) {
             if (result.error) {
                 submitButton.disabled = true;
@@ -79,21 +85,21 @@
                     let form = document.getElementById('payment-form');
                     let url = form.action;
                     let redirect = '/merci';
-
+                    
                     fetch(
-                        url,
-                        {
-                            headers: {
-                                "Content-Type": "application/json",
-                                "Accept": "application/json, text-plain, */*",
-                                "X-Requested-With": "XMLHttpRequest",
-                                "X-CSRF-TOKEN": token
-                            },
-                            method: 'post',
-                            body: JSON.stringify({
-                                paymentIntent: paymentIntent,
-                            })
-                        }).then((data)=>{
+                    url,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json, text-plain, */*",
+                            "X-Requested-With": "XMLHttpRequest",
+                            "X-CSRF-TOKEN": token
+                        },
+                        method: 'post',
+                        body: JSON.stringify({
+                            paymentIntent: paymentIntent,
+                        })
+                    }).then((data)=>{
                         console.log(data)
                         window.location.href = redirect;
                     }).catch((error) => {
