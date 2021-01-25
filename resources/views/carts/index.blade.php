fcd54fb01fe281e88bae0946531a81a73998e3a6
ZiKo <lucas.matas@ynov.com>
2021-01-25 15:28:23 +0100

    dark cart page

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -15,21 +15,21 @@
     <div class="pb-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+                <div class="col-lg-12 p-5 bg-gray2e fs-6 rounded shadow-sm mb-5">
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col" class="border-0 bg-dark">
+                                    <th scope="col" class="border-0 bg-gray1e">
                                         <div class="p-2 px-3 text-uppercase text-white">Produit</div>
                                     </th>
-                                    <th scope="col" class="border-0 bg-dark">
+                                    <th scope="col" class="border-0 bg-gray1e">
                                         <div class="py-2 text-uppercase text-white">Prix</div>
                                     </th>
-                                    <th scope="col" class="border-0 bg-dark">
+                                    <th scope="col" class="border-0 bg-gray1e">
                                         <div class="py-2 text-uppercase text-white">Quantité</div>
                                     </th>
-                                    <th scope="col" class="border-0 bg-dark">
+                                    <th scope="col" class="border-0 bg-gray1e">
                                         <div class="py-2 text-uppercase "></div>
                                     </th>
                                 </tr>
@@ -38,19 +38,19 @@
                                 @foreach (Cart::content() as $product)
                                 
                                 
-                                <tr class="">
-                                    <th scope="row" class="border-bottom-0 border-dark">
+                                <tr class="text-white">
+                                    <th scope="row" class="border-bottom-0 border-primary">
                                         <div class="p-3">
                                             <img src="{{ $product->model->pathImage }}" alt="" width="70" class="img-fluid rounded shadow-sm">
                                             <div class="ml-3 d-inline-block align-middle">
-                                                <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">{{ $product->model->name }}</a></h5>
-                                                <span class="text-muted font-weight-normal font-italic d-block">Category: </span>
+                                                <h5 class="mb-0"> <a href="#" class="text-white fs-5 d-inline-block align-middle">{{ $product->model->name }}</a></h5>
+                                                <span class="text-muted font-weight-normal font-italic d-block">Categorie: {{ $product->model->type }} </span>
                                             </div>
                                         </div>
                                     </th>
-                                    <td class="border-bottom-0 border-dark align-middle"><strong>{{ $product->model->price }} €</strong></td>
-                                    <td class="border-bottom-0 border-dark align-middle"><strong>1</strong></td>
-                                    <td class="border-bottom-0 border-dark align-middle">
+                                    <td class="border-bottom-0 border-primary align-middle"><strong>{{ $product->model->price }} €</strong></td>
+                                    <td class="border-bottom-0 border-primary align-middle"><strong>1</strong></td>
+                                    <td class="border-bottom-0 border-primary align-middle">
                                         <form action="{{ route('carts.destroy',  $product->rowId) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
@@ -72,35 +72,22 @@
                 </div>
             </div>
             
-            <div class="row py-5 p-4 bg-white rounded shadow-sm">
+            <div class="row py-5 p-4 bg-gray2e text-white rounded shadow-sm">
                 <div class="col-lg-6">
-                    <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Coupon code</div>
-                    <div class="p-4">
-                        <p class="font-italic mb-4">If you have a coupon code, please enter it in the box below</p>
-                        <div class="input-group mb-4 border rounded-pill p-2">
-                            <input type="text" placeholder="Apply coupon" aria-describedby="button-addon3" class="form-control border-0">
-                            <div class="input-group-append border-0">
-                                <button id="button-addon3" type="button" class="btn btn-dark px-4 rounded-pill"><i class="fa fa-gift mr-2"></i>Apply coupon</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Instructions for seller</div>
-                    <div class="p-4">
-                        <p class="font-italic mb-4">If you have some information for the seller you can leave them in the box below</p>
-                        <textarea name="" cols="30" rows="2" class="form-control"></textarea>
-                    </div>
+            
+            
                 </div>
-                <div class="col-lg-6">
-                    <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Détails de la commande </div>
+                <div class="col-lg-12">
+                    <div class="bg-gray1e rounded-pill px-4 py-3 text-uppercase font-weight-bold">Détails de la commande </div>
                     <div class="p-4">
-                        <p class="font-italic mb-4">Shipping and additional costs are calculated based on values you have entered.</p>
+                        <p class="font-italic mb-4">Tout les coûts additionnel seront calculé automatiquement.</p>
                         <ul class="list-unstyled mb-4">
-                            <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Sous-total </strong><strong>{{ Cart::subtotal() }} €</strong></li>
-                            <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Tax</strong><strong>{{ Cart::tax() }}</strong></li>
-                            <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total</strong>
+                            <li class="d-flex justify-content-between fs-5 py-3 border-bottom"><strong class="text-muted">Sous-total </strong><strong>{{ Cart::subtotal() }} €</strong></li>
+                            <li class="d-flex justify-content-between fs-5 py-3 border-bottom"><strong class="text-muted">Tax</strong><strong>{{ Cart::tax() }}</strong></li>
+                            <li class="d-flex justify-content-between fs-5 py-3 border-bottom"><strong class="text-muted">Total</strong>
                                 <h5 class="font-weight-bold">{{ Cart::total() }}</h5>
                             </li>
-                        </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block">Passer a la caisse</a>
+                        </ul><a href="#" class="btn btn-primary rounded-pill py-2 fs-6 btn-block">Passer au paiement</a>
                     </div>
                 </div>
             </div>
