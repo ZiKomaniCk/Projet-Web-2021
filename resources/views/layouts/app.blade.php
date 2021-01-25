88846688a9483348061dedbee6825bd672b26dcc
ZiKo <lucas.matas@ynov.com>
2021-01-25 12:57:08 +0100

    LOGO ALPHAWWAAAAAA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>GAMINGUE</title>
+
+    <link rel = "icon" href =  
+    "\images\site\ALPHA.png" 
+        type = "image/x-icon"> 
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
