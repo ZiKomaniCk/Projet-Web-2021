d58b02657f9c27a5be8223600eeb28bc1e4a2af4
Modolo Thomas <thomas.modolo@ynov.com>
2021-01-27 15:54:11 +0100

    cleaning code

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,23 +1,11 @@
 <?php
 
-use Gloudemans\Shoppingcart\Facades\Cart;
-
-// function getPrice($price){
-//     $cent = 100;
-//     $price = floatval($price) * $cent;
-//     // print_r(gettype());
-//     // dd(floatval($price));
-//     return number_format($price, 2, ',', '') . '€';
-// }
-
 function getPriceIntent($price){
-    // dd($price);
     $price = floatval(str_replace(",", "", $price));
     return $price;
 }
 
 function getPriceSolde($price){
-    // dd($price);
     $price = intval(str_replace(",", "", $price));
     return ($price * 10);
 }
