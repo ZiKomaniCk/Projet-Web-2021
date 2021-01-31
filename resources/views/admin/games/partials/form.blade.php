27de4e41f010b21e0841392c3c8b5515afb2c3d6
Modolo Thomas <thomas.modolo@ynov.com>
2021-01-31 15:38:48 +0100

    fixing bugs on forms game

--- resources/views/admin/games/partials/form.blade.php
+++ resources/views/admin/games/partials/form.blade.php
@@ -72,12 +72,12 @@
     <div id="status" class="col-md-6">
         <div class="mt-2">
             <input type="radio" name="visible" value="1"
-            @if ($game ?? '' &&  $game->visible == 1 || $update == 'no') checked @endif>
+            @if (($game ?? '') && ($game->visible == 1) ) checked @endif>
             <label>Visible</label>
         </div>
         <div class="mt-2">
             <input type="radio" name="visible" value="0" 
-            @if ($game ?? '' && $game->visible == 0 ) checked @endif>
+            @if (($game ?? '') && ($game->visible == 0) ) checked @endif>
             <label>Caché</label>
         </div>
     </div>
